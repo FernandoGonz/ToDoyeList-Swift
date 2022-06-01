32669d0802dff1831eae5233eda8c3a053191013
Fernando <fernando@agovest.com>
2022-06-01 17:31:01 -0500

    Implementing CoreDate module to the project. Search item by UISearchBar almost done

--- Todoey/Controllers/ViewController.swift
+++ Todoey/Controllers/ViewController.swift
@@ -6,19 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
     
+    let context: NSManagedObjectContext? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     var itemArray: [Item] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.itemArray.append(Item(title: "Find Mike", done: false))
-        self.itemArray.append(Item(title: "Play Ratchet", done: false))
-        self.itemArray.append(Item(title: "Learn about swift", done: false))
+        self.loadItems()
     }
-
+    
+    /// This function allows to save a new Item into CoreData model
+    /// ```
+    /// print(hello("World")) // Hello, World!
+    /// ```
+    /// - Warning: The returned string is not localized.
+    /// - Parameter subject: The subject to be welcomed.
+    /// - Parameter alfa: This is a dummy parameter
+    /// - Returns: A hello string to the `subject`.
+    private func saveItem() {
+        do {
+            try self.context!.save()
+        } catch {
+            print("Error saving item into DataModel \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    private func loadItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do {
+            self.itemArray = try self.context!.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+    
     //MARK: - Add New Item
     @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
         // Creating local variable
@@ -30,7 +57,12 @@ class ViewController: UITableViewController {
         // Creating action button for the alert
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             // Adding the new value to the itemsArray and reloading data
-            self.itemArray.append(Item(title: textField.text ?? "New Item", done: false))
+            let item: Item = Item(context: self.context!)
+            item.title = textField.text!
+            item.done = false
+            self.itemArray.append(item)
+            
+            self.saveItem()
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -76,13 +108,42 @@ class ViewController: UITableViewController {
         // Changing the done flag
         self.itemArray[indexPath.row].done = !self.itemArray[indexPath.row].done
         
-        // Updating tableView (calling UITableViewDataSource again)
-        tableView.reloadData()
+        // Updating done attribute for current item
+        self.saveItem()
         
         // To deselect a row with animation
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
+//MARK: - UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let item = searchBar.text {
+            // This method is called when users taps magnifier icon or serach button into keyboard is pressed
+            let request: NSFetchRequest<Item> = Item.fetchRequest()
+            
+            // We need to specify how should show the results using filter. If you want te get result with case sensitive use only CONTAINS, for the other hand, use CONTAINS[cd]
+            let predicate: NSPredicate = NSPredicate(format: "title CONTAINS[cd] %@", item)
+            
+            // Adding query for the request
+            request.predicate = predicate
+            
+            // Sorting results from the request
+            let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+            
+            request.sortDescriptors = [sortDescriptor]
+            
+            // Updating data from the context (BD) with the filter
+            do {
+                try self.context!.save()
+            } catch {
+                print("Error saving item into DataModel \(error)")
+            }
+            
+            tableView.reloadData()
+        }
+    }
+}
 
 
